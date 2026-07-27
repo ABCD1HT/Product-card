e7224fc4c49580cb33cff2620b1be611fa31d109
ABCD <abdulmuktadirlugarvol@gmail.com>
2026-07-27 18:16:18 +0300

    feature/06-homework

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+const productCards = document.querySelectorAll('.card-container');
+const changeColorAllCardButton = document.querySelector('#change-color-all-card');
+const greenColorHash = '#00FF00';
+const blueColorHash = '#0000FF';
+
+changeColorAllCardButton.addEventListener('click', () => {
+  productCards.forEach((card) => card.style.backgroundColor = greenColorHash)
+})
+
+
+const firstProductCard = document.querySelector('.card-container');
+const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
+
+changeColorFirstCardButton.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = blueColorHash;
+})
+
+
+const openGoogleButton = document.querySelector('#open-google');
+
+openGoogleButton.addEventListener('click', openGoogle)
+
+function openGoogle() {
+  const answer = confirm('Вы действительно хотите открыть Google?');
+
+  if (answer === true) {
+    window.open('https://google.com')
+  } else {
+    return;
+  }
+}
+
+
+const outputLogButton = document.querySelector('#output-console-log');
+
+outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №4'))
+
+function outputConsoleLog(message) {
+  alert(message)
+  console.log(message)
+}
